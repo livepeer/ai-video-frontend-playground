aad03c558e365c093e92f3d48a8748edb99f23a7
Yondon Fu <yondon.fu@gmail.com>
2024-01-26 16:59:14 -0500

    text-to-image: Support extra params

---
+++ src/app/text-to-image/actions.ts
@@ -0,0 +1,26 @@
+"use server";
+
+export async function textToImage(prevState: any, formData: FormData) {
+  const modelID = formData.get("model_id");
+  const prompt = formData.get("prompt");
+  const height = formData.get("height");
+  const width = formData.get("width");
+
+  const data = {
+    model_id: modelID ? modelID : "",
+    prompt,
+    height: height ? Number(height) : null,
+    width: width ? Number(width) : null,
+  };
+
+  const url = process.env.AI_VIDEO_API_URL + "/text-to-image";
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  return await res.json();
+}
--- src/app/text-to-image/page.tsx
+++ src/app/text-to-image/page.tsx
@@ -3,46 +3,100 @@
 import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { styled } from "@mui/material/styles";
+import { useFormStatus } from "react-dom";
+import { useFormState } from "react-dom";
+import { textToImage } from "./actions";
+
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
+
+const MODELS = [
+  "stabilityai/sd-turbo",
+  "runwayml/stable-diffusion-v1-5",
+  "stabilityai/stable-diffusion-xl-base-1.0",
+  "prompthero/openjourney-v4",
+];
 
 const TextToImageBox = () => {
-  const [prompt, setPrompt] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+  const [modelID, setModelID] = useState(MODELS[0]);
+  const { pending } = useFormStatus();
+  const [imageResponse, formAction] = useFormState(textToImage, null);
 
-  const onChange = (event: any) => {
-    setPrompt(event.target.value);
-  };
-
-  const onSubmit = async () => {
-    const res = await fetch("/api/text-to-image", {
-      method: "POST",
-      body: JSON.stringify({
-        prompt: prompt,
-        model_id: "stabilityai/sd-turbo",
-      }),
-    });
-
-    const data = await res.json();
-
-    if (data.images.length > 0) {
-      setImageUrl(data.images[0].url);
-    }
+  const handleModelIDChange = (event: any) => {
+    setModelID(event.target.value as string);
   };
 
   return (
-    <div className="flex flex-col items-center justify-center p-24 space-y-4">
+    <form
+      className="flex flex-col items-center justify-center p-24 space-y-4"
+      action={formAction}
+    >
       <TextField
         className="w-1/4"
         id="outlined-multiline-static"
         label="prompt"
         multiline
         rows={4}
-        onChange={onChange}
+        name="prompt"
+        type="text"
       />
-      <Button component="label" variant="contained" onClick={onSubmit}>
+      <Box sx={{ minWidth: 300 }}>
+        <FormControl fullWidth>
+          <InputLabel>Model ID</InputLabel>
+          <Select
+            label="model_id"
+            name="model_id"
+            value={modelID}
+            type="text"
+            onChange={handleModelIDChange}
+          >
+            {MODELS.map((modelID) => (
+              <MenuItem key={modelID} value={modelID}>
+                {modelID}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+      <TextField
+        id="outlined-basic"
+        label="height"
+        variant="outlined"
+        name="height"
+        type="number"
+      />
+      <TextField
+        id="outlined-basic"
+        label="width"
+        variant="outlined"
+        name="width"
+        type="number"
+      />
+      <Button component="label" variant="contained" disabled={pending}>
         Generate Image
+        <VisuallyHiddenInput type="submit" />
       </Button>
-      <div>{imageUrl && <img src={imageUrl} />}</div>
-    </div>
+      {imageResponse && (
+        <div>
+          <img src={imageResponse.images[0].url} />
+        </div>
+      )}
+    </form>
   );
 };
 
